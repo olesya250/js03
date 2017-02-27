89518893de7fc57632e2fcc79d2b3402d9aa4a11
Olesia <olesyasheremeteva@i.ua>
2017-02-27 13:03:21 +0000

    Comment

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+function fib(n){
+	if (n==1||n==2) return 1;
+	else return fib(n-2)+fib(n-1);		
+	}
+	var userNum=+prompt('Введіть порядковий номер числа Фібоначчі');
+alert(fib(userNum));
+   
+   function fib1(n) {
+  var a = 1,
+    b = 1;
+  for (var i = 3; i <= n; i++) {
+    var c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+ var userNum1=+prompt('Введіть порядковий номер числа  Фібоначчі');
+alert(fib1(userNum1));
